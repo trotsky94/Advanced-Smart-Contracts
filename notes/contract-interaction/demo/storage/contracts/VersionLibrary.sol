e2161246fe47457141202e2e82d8abdc9f47aa7f
dhruvinparikh93 <imsilent93@gmail.com>
2020-07-16 21:51:16 -0400

    husky test commit

--- notes/contract-interaction/demo/storage/contracts/VersionLibrary.sol
+++ notes/contract-interaction/demo/storage/contracts/VersionLibrary.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract VersionLibrary {
     uint public version = 1;
 
-    function setVersion(uint newVersion) public {
+    function setVersion(uint newVersion)   public {
         version = newVersion;
     }
 }
