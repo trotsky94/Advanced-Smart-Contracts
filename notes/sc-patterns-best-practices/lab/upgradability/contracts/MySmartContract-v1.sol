e4f85edc47993098f7cc3b79878179ae8ad2edc5
dhruvinparikh93 <imsilent93@gmail.com>
2020-07-24 14:59:36 -0400

    add upgradability lab

---
+++ notes/sc-patterns-best-practices/lab/upgradability/contracts/MySmartContract-v1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+
+/**
+ * @dev Renamed version 1 to MySmartContractV1 to quickly workaround Truffle's singleton feature when deploying contracts.
+ * This allows us to have both contract versions deployed at the same time.
+ */
+contract MySmartContractV1 {
+    uint32 public counter;
+    address private owner;
+
+    /**
+    @dev Enforces the caller to be the contract's owner.
+    */
+    modifier isOwner {
+        require(msg.sender == owner, "Sender is not owner.");
+        _;
+    }
+
+    constructor() public {
+        counter = 0;
+        // Sets the contract's owner as the address that deployed the contract.
+        owner = msg.sender;
+    }
+
+    /**
+    @notice Increments the contract's counter if contract is active.
+    @dev It should revert if the contract is stopped. Create a modifier named "isNotStopped"
+    */
+    function incrementCounter() public {
+        counter += 2; // This is an intentional bug.
+    }
+}
