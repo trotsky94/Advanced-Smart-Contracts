4601dc30feb720ba5511478974a921147c488210
dhruvinparikh93 <imsilent93@gmail.com>
2020-08-11 21:43:38 -0400

    lint applied

--- notes/contract-interaction/demo/storage/contracts/Delegator.sol
+++ notes/contract-interaction/demo/storage/contracts/Delegator.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >0.4.0 <0.7.0;
 
 contract Delegator {
-    bytes1    public val1;   // storage slot 0
-    uint8     public val2;   // storage slot 0
-    bool      public val3;   // storage slot 0   
-    address   public val4;   // storage slot 0
-    string    public val5;   // storage slot 1
-    
+    bytes1 public val1;   // storage slot 0
+    uint8 public val2;   // storage slot 0
+    bool public val3;   // storage slot 0
+    address public val4;   // storage slot 0
+    string public val5;   // storage slot 1
     address public owner;    // storage slot 2
     address public delegate;  // contract to delegate calls to
 
@@ -19,6 +19,7 @@ contract Delegator {
 
     function setVersion(uint newVersion) public {
         bytes memory abiData = abi.encodeWithSignature("setVersion(uint256)", newVersion);
+        /* solium-disable-next-line security/no-low-level-calls*/
         (bool success, ) = delegate.delegatecall(abiData);
         require(success, "external call failed");
     }
