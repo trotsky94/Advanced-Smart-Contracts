290a0ea6ae6fc6ae550961116849b1503ad49291
dhruvinparikh <imsilent93@gmail.com>
2021-03-16 17:40:07 -0400

    add contract artefacts

--- notes/storage-exception-debugging/demo/storage/migrations/2_deploy_contracts.js
+++ notes/storage-exception-debugging/demo/storage/migrations/2_deploy_contracts.js
@@ -1,7 +1,14 @@
-const Delegator = artifacts.require('Delegator');
-const VersionLibrary = artifacts.require('VersionLibrary');
+const Delegator = artifacts.require("Delegator");
+const VersionLibrary = artifacts.require("VersionLibrary");
 
-module.exports = function(deployer) {
-    deployer.deploy(VersionLibrary)
-    .then(() => deployer.deploy(Delegator, VersionLibrary.address,"0x70617373776f7264000000000000000000000000000000000000000000000000"));
-}
+module.exports = function (deployer) {
+  deployer
+    .deploy(VersionLibrary)
+    .then(() =>
+      deployer.deploy(
+        Delegator,
+        VersionLibrary.address,
+        web3.utils.asciiToHex("password")//"0x70617373776f7264000000000000000000000000000000000000000000000000"
+      )
+    );
+};
