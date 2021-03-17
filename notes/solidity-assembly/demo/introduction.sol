a9cf9e41ed8dc69ba14e1eea59fec323012b7628
dhruvinparikh <imsilent93@gmail.com>
2021-03-17 16:02:27 -0400

    upgrade solidity compiler in intro asm code

--- notes/solidity-assembly/demo/introduction.sol
+++ notes/solidity-assembly/demo/introduction.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >0.4.0 <0.7.0;
+pragma solidity ^0.7.1;
 
 contract Sample {
-    
-    function helloFn() public  pure returns(string memory name){
+    function helloFn() public pure returns(string memory name) {
         assembly {
-            let x := 1
+            let x:= 1
             let y := "Hello World"
             name := y
         }
@@ -22,26 +21,31 @@ contract Sample {
         }
     }
     
-    function fnSwitch(uint input)  public pure returns(uint b){
+    function fnSwitch(uint input) public pure returns(uint b) {
         assembly {
             switch input
-                case 0 { b := 12 }
-                case 1 { b := 13 }
-                case 100 { b := 13 }
-                default { b := add(input,1) } // mul, div, sub
+                case 0 {
+                    b := 12
+                }
+                case 1 {
+                    b := 13
+                }
+                default {
+                    b := add(input,1) // mul, div, sub
+                }
         }
     }
     
     function fnLoop(uint max) public pure returns(uint result) {
         assembly {
-            for { let i := 0 }
-             lt(i, 20)
-             { i := add(i,1) }
-             {
-                 if lt(i,3) { continue }
-                 if gt(i , max) { break }
-                 result := add(result,1)
-             }
+            for {let i := 0 }
+            lt(i, 20)
+            { i := add(i,1)} // add(a,b)
+            {
+                if lt(i,3) { continue }
+                if gt(i, max) {break}
+                result := add(result,1)
+            }
         }
     }
     
@@ -51,43 +55,45 @@ contract Sample {
         }
     }
     
-    function srTest(uint8 x) public pure returns(uint8  result) {
+    function srTest(uint8 x) public pure returns(uint8 result) {
         assembly {
             result := shr(1,x)
         }
+        /**
+         * Explanation: shift right
+         * 00001111 => 15
+         * \\\\\\\\\
+         *  00000111- => 7
+         */ 
     }
     
-    function countUnsetBit(uint8 data) public pure returns (uint8 result) {
-        for(uint i = 0; i < 8 ; i = i + 1) {
-            if ( ((data >> i) & 1) == 0){
-                result += 1;
+    function countUnsetBit(uint8 data) public pure returns(uint8 result) {
+        for(uint i = 0; i < 8 ; i++) {
+            if(((data >> i) & 1) == 0) {
+                result++;
             }
         }
+        /**
+         * Explanation and operation
+         * num1 = 00001111
+         * operation = and
+         * num2 = 10101010
+         * result = 0000010
+         */
     }
     
-    function countUnsetBitInAsm(uint8 data) public pure returns (uint8 result) {
+    function countUnsetBitInAsm(uint8 data) public pure returns(uint8 result) {
         assembly {
             for {let i := 0 }
-             lt(i,8)
-            { i := add(i,1)}{
-                let byteShifted := shr(i, data)
-                let andedResult := and(byteShifted,1)
-                if eq(andedResult,0){
+            lt(i,8)
+            {i := add(i,1)} 
+            {
+                let bytesShifted := shr(i,data)
+                let andResult := and(bytesShifted,1)
+                if eq(andResult,0) {
                     result := add(result,1)
                 }
             }
         }
     }
-    
-    
- }
-
- // 00001111 => 15
- // 00000111 => 7
- 
- // num1 00001111
- // num2 10101010
- // res  00001010
- 
- 
- 
+}
