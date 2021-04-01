5ce83727ee7e147f0dcc0e1a195c21a9349a98bc
dhruvinparikh <imsilent93@gmail.com>
2021-04-01 12:12:14 -0400

    clean up and re-org

--- notes/sc-patterns-best-practices/lab/upgradability/migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const MySmartContractV1 = artifacts.require("MySmartContractV1");
-const MySmartContractV2 = artifacts.require("MySmartContract");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(MySmartContractV1);
-  deployer.deploy(MySmartContractV2, 100);
-};
